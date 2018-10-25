f806687dd9791796fc08123032933672d924e2d4
CodingMa <897566085@qq.com>
2018-10-25 16:22:26 +0800

    add knn

---
+++ Chapter-02/kNN.py
@@ -0,0 +1,11 @@
+import numpy as np
+import operator
+
+def createDataSet():
+    group = np.array([[1.0, 1.1],[1.0, 1.0],[0, 0],[0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+group, labels = createDataSet();
+print(group)
+print(labels)
