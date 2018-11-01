78d3b22c74b49a7e30cc40b3562f71b8be40445a
CodingMa <897566085@qq.com>
2018-11-01 16:51:11 +0800

    update kNN

--- Chapter-02/kNN.py
+++ Chapter-02/kNN.py
@@ -2,8 +2,9 @@ import numpy as np
 import operator
 from os import listdir
 
+
 def createDataSet():
-    group = np.array([[1.0, 1.1],[1.0, 1.0],[0, 0],[0, 0.1]])
+    group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
@@ -19,24 +20,26 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    #sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    # sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
 
 def file2matrix(filename):
     fr = open(filename)
     numberOfLines = len(fr.readlines())
-    returnMat = np.zeros((numberOfLines,3))
+    returnMat = np.zeros((numberOfLines, 3))
     classLabelVector = []
     fr = open(filename)
     index = 0
     for line in fr.readlines():
         line = line.strip()
         listFromLine = line.split('\t')
-        returnMat[index,:] = listFromLine[0:3]
+        returnMat[index, :] = listFromLine[0:3]
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
-    return returnMat,classLabelVector
+    return returnMat, classLabelVector
+
 
 def autoNorm(dataSet):
     minVals = dataSet.min(0)
@@ -44,34 +47,37 @@ def autoNorm(dataSet):
     ranges = maxVals - minVals
     normDataSet = np.zeros(np.shape(dataSet))
     m = dataSet.shape[0]
-    normDataSet = dataSet - np.tile(minVals, (m,1))
-    normDataSet = normDataSet/np.tile(ranges, (m,1))
+    normDataSet = dataSet - np.tile(minVals, (m, 1))
+    normDataSet = normDataSet / np.tile(ranges, (m, 1))
     return normDataSet, ranges, minVals
+
 
 def datingClassTest():
     hoRatio = 0.50
-    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
     normMat, ranges, minVals = autoNorm(datingDataMat)
     m = normMat.shape[0]
-    numTestVecs = int(m*hoRatio)
+    numTestVecs = int(m * hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:], datingLabels[numTestVecs:m],3)
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
 
         print("He is %d years old" % (25))
-        print ("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
         if (classifierResult != datingLabels[i]): errorCount += 1.0
-    print("the total error rate is: %f" % (errorCount/float(numTestVecs)))
+    print("the total error rate is: %f" % (errorCount / float(numTestVecs)))
     print(errorCount)
 
+
 def img2vector(filename):
-    returnVect = np.zeros((1,1024))
+    returnVect = np.zeros((1, 1024))
     fr = open(filename)
     for i in range(32):
         lineStr = fr.readline()
         for j in range(32):
-            returnVect[0,32*i+j] = int(lineStr[j])
+            returnVect[0, 32 * i + j] = int(lineStr[j])
     return returnVect
+
 
 def handwritingClassTest():
     hwLabels = []
@@ -93,10 +99,20 @@ def handwritingClassTest():
         classNumStr = int(fileStr.split('_')[0])
         vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
         classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
-        print ("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
         if (classifierResult != classNumStr): errorCount += 1.0
-    print ("the total number of errors is: %d" % errorCount)
-    print ("the total error rate is: %f" % (errorCount/float(mTest)))
+    print("the total number of errors is: %d" % errorCount)
+    print("the total error rate is: %f" % (errorCount / float(mTest)))
 
 
-handwritingClassTest()
+def test():
+    # group, labels = createDataSet()
+    # print(group, labels)
+    # print(classify0([0,0], group, labels, 3))
+    # dataDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    # autoNorm(dataDataMat)
+    handwritingClassTest()
+
+
+if __name__ == '__main__':
+    test()
